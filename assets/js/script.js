131b29c6a01ed976ba48a29ec0a1e6fa2075d4e5
VIVEK MANGAL <138361918+vivekmangal204@users.noreply.github.com>
2024-04-15 09:08:37 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "Full Stack Web Development", "Data Analysis", "Machine Learning"],
+    strings: ["frontend development", "backend development", "web designing", "Full Stack Web Development", "DSA Using C++"],
     loop: true,
     typeSpeed: 60,
     backSpeed: 30,
